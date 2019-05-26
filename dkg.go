28976892c046ce303d10f8364c00c239487e511c
Evgeny Danienko <6655321@bk.ru>
2019-05-26 12:21:54 +0300

    remove debug

--- dkg.go
+++ dkg.go
@@ -564,12 +564,10 @@ func (i *DKGInstance) Run() error {
 	}
 	signersLock.Unlock()
 
-	var closerFunc func()
-
 	for {
 		switch i.State {
 		case STATE_PUBKEY_SEND:
-			i.Streamer, closerFunc = GenerateStream(i.Server, signers, i.SignerIdx, "pubkey")
+			i.Streamer, _ = GenerateStream(i.Server, signers, i.SignerIdx, "pubkey")
 			time.Sleep(2 * time.Second)
 
 			err := i.SendPubkey()
@@ -588,10 +586,9 @@ func (i *DKGInstance) Run() error {
 				panic(err)
 			}
 
-			closerFunc()
 			i.moveToState(STATE_SEND_DEALS)
 		case STATE_SEND_DEALS:
-			i.Streamer, closerFunc = GenerateStream(i.Server, signers, i.SignerIdx, "deals")
+			i.Streamer, _ = GenerateStream(i.Server, signers, i.SignerIdx, "deals")
 			time.Sleep(2 * time.Second)
 
 			err := i.SendDeals()
@@ -609,10 +606,9 @@ func (i *DKGInstance) Run() error {
 				panic(err)
 			}
 
-			closerFunc()
 			i.moveToState(STATE_SEND_RESPONSES)
 		case STATE_SEND_RESPONSES:
-			i.Streamer, closerFunc = GenerateStream(i.Server, signers, i.SignerIdx, "responses")
+			i.Streamer, _ = GenerateStream(i.Server, signers, i.SignerIdx, "responses")
 			time.Sleep(2 * time.Second)
 
 			err := i.SendResponses()
@@ -640,10 +636,9 @@ func (i *DKGInstance) Run() error {
 				panic(err)
 			}
 
-			closerFunc()
 			i.moveToState(STATE_PROCESS_Commits)
 		case STATE_PROCESS_Commits:
-			i.Streamer, closerFunc = GenerateStream(i.Server, signers, i.SignerIdx, "commits")
+			i.Streamer, _ = GenerateStream(i.Server, signers, i.SignerIdx, "commits")
 			time.Sleep(2 * time.Second)
 
 			err := i.ProcessCommits()
@@ -653,7 +648,6 @@ func (i *DKGInstance) Run() error {
 				panic(err)
 			}
 
-			closerFunc()
 			i.moveToState(STATE_PROCESS_Complaints)
 		case STATE_PROCESS_Complaints:
 			err := i.ProcessComplaints()
